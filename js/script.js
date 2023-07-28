861af2cd675060b1315766892b7d79b30a948632
namikama <139845247+namikama@users.noreply.github.com>
2023-07-28 13:26:10 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// toggle kelas aktif
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+const menu = document.querySelector('#menu');
+document.addEventListener('click', function(e){
+  if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
+  }
+});
